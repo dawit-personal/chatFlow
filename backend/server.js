0990c4be2409011dca26f18d2a98ea0f76d6c810
dawitfrazer <sdfrazer@gmail.com>
2025-06-05 12:32:48 -0400

    test commit

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const app = require('./src/app');
 
 const PORT = process.env.BACKEND_PORT || 4000;
 
+
 app.listen(PORT, () => {
   console.log('PORT from .env:', process.env.BACKEND_PORT);
   console.log(`🚀 Server is running at http://localhost:${PORT}`);
