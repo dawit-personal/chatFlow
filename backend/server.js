7f3f439147c4e976599443fb11aa2ff3ebd8937d
dawitfrazer <sdfrazer@gmail.com>
2025-06-05 11:09:50 -0400

    fix: fix the port issue

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 const app = require('./src/app');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.BACKEND_PORT || 4000;
 
 app.listen(PORT, () => {
-  console.log('PORT from .env:', process.env.PORT);
+  console.log('PORT from .env:', process.env.BACKEND_PORT);
   console.log(`🚀 Server is running at http://localhost:${PORT}`);
 });
